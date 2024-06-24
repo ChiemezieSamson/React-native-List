c925d93010e0ee0e11f8e982e828a19ff96ac15b
nebeolisa chiemezie samson <daddygreenarrow44@gmail.com>
2024-06-24 22:35:40 +0900

    react native list

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView, ScrollView, StyleSheet, Text, View, StatusBar } from 'react-native';
+import PokemonList from "./Data.json"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollview}>
+        {PokemonList?.map(pokemon => {
+          return (
+            <View key={pokemon.id} style={styles.card}>
+              <Text style={styles.CardText}>Text{pokemon.type}</Text>
+              <Text style={styles.CardText}>{pokemon.name}</Text>
+            </View>
+          )
+        })}
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f5f5f5',
+    paddingTop: StatusBar.currentHeight
+  }, 
+  scrollview: {
+    paddingHorizontal: 16
   },
+  card: {
+    backgroundColor: "white",
+    padding: 16,
+    borderRadius: 8,
+    borderWidth: 1,
+    marginBottom: 16
+  },
+  CardText: {
+    fontSize: 20
+  }
 });
